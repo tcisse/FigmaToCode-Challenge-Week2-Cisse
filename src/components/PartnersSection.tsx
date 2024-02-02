038c361c37a6a189cabdef98ce0cae97c7295ff8
tairoucisse <cisse.amidou@axazara.io>
2024-02-02 11:25:41 +0100

    - update partners sections design

--- src/components/PartnersSection.tsx
+++ src/components/PartnersSection.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function PartnersSection() {
   return (
     <div className="">
-      <div className="grid lg:grid-cols-6 md:grid-cols-3 gap-4 grid-cols-2 py-10 lg:max-w-7xl mx-auto">
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-4 grid-cols-2 py-10 lg:max-w-7xl mx-auto">
         <div>
           <Image src="/assets/logo/01.svg" width={200} height={70} alt="" />
         </div>
